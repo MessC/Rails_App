200869ec68c03c1038dff85e6f7ce2069110a9e7
Messenger, C <MessC@noreply.guthub.com>
2016-04-26 09:37:27 -0400

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post_via_redirect users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+		end
+    assert_template 'users/show'
+  end
+end
